b6252b072d20abc592a449930c49eb49b2466385
wittawin381 <31582969+wittawin381@users.noreply.github.com>
2017-09-03 19:49:24 +0700

    new

--- wordlist.js
+++ wordlist.js
@@ -7,6 +7,14 @@ function main(){
     $('.page').hover(function(){
         $('.panel').slideUp(400);
     })
+    
+    $('.menu2').hide();
+    $('.toggle-nav').on('click',function(){
+        $('.menu2').slideToggle();
+    })
+    $('.showBut2').on('click',function(){
+        $('.panel').stop(true, true).slideDown(400);
+    })
 }
 
 
